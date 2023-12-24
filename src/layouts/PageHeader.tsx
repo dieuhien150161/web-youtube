b06d2e0448bf91f9b1e443ebbf992781d04d8aec
vtdhien <vtdhien@cmcglobal.vn>
2023-12-24 23:41:54 +0700

    feat: coding header

---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,29 @@
+import { Bell, Menu, Upload, User } from "lucide-react";
+import logo from "../assets/Logo.png";
+import { Button } from "../components/Button";
+export function PageHeader() {
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logo} alt="" className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button size="icon" variant="ghost">
+          <Upload />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User />
+        </Button>
+      </div>
+    </div>
+  );
+}
